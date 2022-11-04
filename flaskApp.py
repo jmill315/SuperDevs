35faac68be0d6183b17fc31b9ac373edcd135cf3
Harrison Whetzel <hwhetzel@uncc.edu>
2022-11-04 14:39:23 -0400

    changed the flask file name and fixed some of the code

--- flaskApp.py
+++ flaskApp.py
@@ -11,8 +11,12 @@ def index():
 
 @app.route('/projects')
 def index():
-    return render_template('index.html')
+    return render_template('projects.html')
 
-@app.route('/')
+@app.route('/projects/<project_id>')
 def index():
-    return render_template('index.html')
+    return render_template('tasks.html')
+
+
+
+app.run(host=os.getenv('IP', '127.0.0.1'), port=int(os.getenv('PORT', 5000)), debug=True)
