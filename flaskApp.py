6b48bbfc8c0f8518e08dbd01561d54dae5372eb2
Harrison Whetzel <hwhetzel@uncc.edu>
2022-11-09 13:26:30 -0500

    added db connection to app

--- flaskApp.py
+++ flaskApp.py
@@ -4,8 +4,17 @@ from flask import Flask  # Flask is the web app that we will customize
 from flask import render_template
 from flask import request
 from flask import redirect, url_for
+from database import db
 
 app = Flask(__name__)
+
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///flask_project_app.db'
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS']=False
+# Bind SQLAlchemy db object to this Flask app
+db.init-app(app)
+# setup models
+with app.app_context():
+    db.create_all()
 
 #test dictionaries and user
 projects = {1: {'title': 'Create web page', 'text': 'Create a web page for client', 'date': '10-1-2020'},
