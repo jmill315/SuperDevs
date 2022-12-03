4fa28ab00ef1be8a9892da0a9feb755295bed527
Jonathan <jmill315@uncc.edu>
2022-12-02 21:50:53 -0500

    Fixed css, images, and started live chat

---
+++ static/scripts/activity.js
@@ -0,0 +1,16 @@
+// $('.question1').click(function(){
+//   alert('test');
+// })
+
+
+window.onload = function(){
+
+  const question1 = document.querySelector('question1');
+  const question2 = document.querySelector('question2');
+  const question3 = document.querySelector('question3');
+
+
+question1.addEventListener("click",function() {
+  alert('test');
+});
+};
